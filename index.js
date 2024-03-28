a93017892012f139b22aac6d317a0337b64700f5
Long <daobaolong2017@gmail.com>
2024-03-28 10:28:59 +0700

    api login

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let apiUser = "http://localhost:3000/user";
 
 //login
-const accountNumber = document.querySelector("accountNumber");
-const bntLogin = document.querySelector("login__signInButton");
+const accountNumber = document.getElementById("accountNumber");
+const bntLogin = document.getElementById("signInButton");
 
 // get user
 const getUser = async () => {
@@ -12,21 +12,23 @@ const getUser = async () => {
 };
 
 // login
-bntLogin.addEventListener("click", (e) => {
+bntLogin.addEventListener("click", function (e) {
   e.preventDefault();
-  if (username.accountNumber == "") {
+  if (accountNumber.value == "") {
     alert("Vui long nhap so tai khoan");
   } else {
     getUser().then((data) => {
       const user = data.find(
-        (user) =>
-          user.accountNumber == accountNumber
+        (user) => user.accountNumber == accountNumber.value
       );
       if (user) {
+        // login success
         alert("Login success");
         window.location.href = "filled.html";
       } else {
-        alert("Login failed");
+        // login failed
+        alert("Login failed. Please try again.");
+        window.location.href = "login.html";
       }
     });
   }
--- script.js
+++ script.js
@@ -5,23 +5,3 @@ checker.onchange = function() {
   sendbtn.disabled = false;
 };
 
-let apiUser = "http://localhost:3000/user";
-
-function login() {
-  getUser(handleLogin)
-}
-
-function getUser(callback) {
-  fetch(apiUser).then(function(res){
-    return res.json().then(callback);
-  });
-}
-function handleLogin(data) {
-  let accountNumber = document.getElementById('accountNumber').value;
-  data.forEach(data =>{
-    if(data.accountNumber === accountNumber){
-      alert("Login successful");
-      window.location.href = "filled.html";
-    }
-  });
-}
