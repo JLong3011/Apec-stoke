1963b55984d22e7d3aa592f9829f81ffe4a07326
Long <daobaolong2017@gmail.com>
2024-04-09 09:29:45 +0700

    update default.html

--- default.js
+++
@@ -1,7 +0,0 @@
-var checker = document.getElementById('checkMe');
-var sendbtn = document.getElementById('hasChecked');
-hasChecked.disabled = true;
-checker.onchange = function() {
-  sendbtn.disabled = false;
-};
-
